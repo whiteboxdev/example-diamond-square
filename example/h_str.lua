90022b61d92fca5f5c7d5a0a93cb98eb5ac7ff25
Klayton Kowalski <klaytonkowalski@gmail.com>
2021-01-04 13:27:13 -0600

    Added diamond-square implementation, added tilesource swapping, added controls one through four, other tweaks.

--- example/h_str.lua
+++ example/h_str.lua
@@ -5,5 +5,9 @@ h_str.use_fixed_fit_projection = hash("use_fixed_fit_projection")
 h_str.button_right = hash("button_right")
 h_str.button_left = hash("button_left")
 h_str.acquire_input_focus = hash("acquire_input_focus")
+h_str.one = hash("one")
+h_str.two = hash("two")
+h_str.three = hash("three")
+h_str.four = hash("four")
 
 return h_str
